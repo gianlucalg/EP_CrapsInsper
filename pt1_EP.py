45e660b48cae753e77d954a5811741eb4af9f587
gianlucalg <gianlucalg@al.insper.edu.br>
2020-04-02 17:56:55 -0300

    Versao Final, com bugs arrumados

--- pt1_EP.py
+++ pt1_EP.py
@@ -1,121 +1,101 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 30 17:20:43 2020
-
-@author: gabrielsalvatorbenatar
-"""
-
 import random
-
-# Função "Twelve", está aposta pode ser feita em qualquer hora do jogo.
-
-def twelve (aposta,dinheiro):
-    
-    dado1 = random.randint(1,6)
-    dado2 = random.randint(1,6)
-    soma_dados = dado1 + dado2
-
-    if soma_dados == 12:
-        aposta = aposta*30
-    else:
-        aposta = 0
-    
-    return aposta
-
-
-# Função "Any Craps", está aposta pode ser feita em qualquer hora do jogo.
-
-def any_craps (aposta,dinheiro):
-    
-    dado1 = random.randint(1,6)
-    dado2 = random.randint(1,6)
-    soma_dados = dado1 + dado2
-    
-    print("A soma dos dados deu {0}".format(soma_dados))
-
-    if soma_dados == 2 or soma_dados == 3 or soma_dados == 12:
-        dinheiro += aposta*7
-        print('Ganhou, a soma deu {0}'.format(soma_dados))
-        print("Você tem {0} reais".format(dinheiro))
-    else:
-        dinheiro-= aposta
-        print('Perdeu a soma deu {0}'.format(soma_dados))
-        print("Você tem {0} reais".format(dinheiro))
-    
-    return dinheiro
-
-
-# Função "Field", está aposta pode ser feita em qualquer hora do jogo.
-
-def field (aposta,dinheiro):
-    
-    dado1 = random.randint(1,6)
-    dado2 = random.randint(1,6)
-    soma_dados = dado1 + dado2
-    
-    print("A soma dos dados deu {0}".format(soma_dados))
-
-    if soma_dados == 5 or soma_dados == 6 or soma_dados == 7 or soma_dados == 8:
-        dinheiro -=aposta
-        print('Perdeu a soma deu {0}'.format(soma_dados))
-        print("Você tem {0} reais".format(dinheiro))
-        
-    elif soma_dados == 3 or soma_dados == 4 or soma_dados == 9 or soma_dados == 10 or soma_dados == 11:
-        dinheiro += aposta
-        print('Ganhou, a soma deu {0}'.format(soma_dados))
-        print("Você tem {0} reais".format(dinheiro))
-        
-    elif soma_dados == 2:
-        dinheiro += aposta*2
-        print('Ganhou, a soma deu {0}'.format(soma_dados))
-        print("Você tem {0} reais".format(dinheiro))
-        
-    elif soma_dados == 12:
-        dinheiro += aposta*3
-        print('Ganhou, a soma deu {0}'.format(soma_dados))
-        print("Você tem {0} reais".format(dinheiro))
-
-    return dinheiro
-
-
-
-
 
 dinheiro = 100
 print("Você tem {0} reais".format(dinheiro))
 
 
 while dinheiro > 0:
-    
+     
     dado1 = random.randint(1,6)
     dado2 = random.randint(1,6)
     soma_dados = dado1 + dado2
+
+    if dinheiro > 0:
+        opcoes = input("Você quer apostar em field, any_craps ou twelve?")
     
-    opcoes = input("Você quer apostar em field, any_craps ou twelve?")
-    
-    if opcoes == 'sim' or opcoes == 'Sim':
+    while opcoes == 'sim' and dinheiro > 0:
         qual = input('No que você quer apostar? (f/ac/t) ')
         if qual == 'f':
             aposta = int(input("Quanto você quer apostar?"))
-            print(field(aposta,dinheiro))
-            opcoes = input("Você quer apostar em field, any_craps ou twelve?")
+            
+
+            dado1_f = random.randint(1,6)
+            dado2_f = random.randint(1,6)
+            soma_dados_f = dado1_f + dado2_f
+
+            if soma_dados_f == 5 or soma_dados_f == 6 or soma_dados_f == 7 or soma_dados_f == 8:
+                dinheiro -=aposta
+                print('Perdeu, a soma deu {0}'.format(soma_dados_f))
+                print("Você tem {0} reais".format(dinheiro))
+        
+            elif soma_dados_f == 3 or soma_dados_f == 4 or soma_dados_f == 9 or soma_dados_f == 10 or soma_dados_f == 11:
+                dinheiro += aposta
+                print('Ganhou, a soma deu {0}'.format(soma_dados_f))
+                print("Você tem {0} reais".format(dinheiro))
+        
+            elif soma_dados_f == 2:
+                dinheiro += aposta*2
+                print('Ganhou, a soma deu {0}'.format(soma_dados_f))
+                print("Você tem {0} reais".format(dinheiro))
+        
+            elif soma_dados_f == 12:
+                dinheiro += aposta*3
+                print('Ganhou, a soma deu {0}'.format(soma_dados_f))
+                print("Você tem {0} reais".format(dinheiro))
+            
+            if dinheiro > 0:
+                opcoes = input("Você quer apostar em field, any_craps ou twelve?")
+
+
         
         elif qual == 'ac':
             aposta = int(input("Quanto você quer apostar?"))
-            print(any_craps(aposta,dinheiro))
-            opcoes = input("Você quer apostar em field, any_craps ou twelve?")
             
-        
-            
-            
-            
-            
-          
-            
-            
-            
-            
+
+            dado1_ac = random.randint(1,6)
+            dado2_ac = random.randint(1,6)
+            soma_dados_ac = dado1_ac + dado2_ac
+
+
+            if soma_dados_ac == 2 or soma_dados_ac == 3 or soma_dados_ac == 12:
+                dinheiro += aposta*7
+                print('Ganhou, a soma deu {0}'.format(soma_dados_ac))
+                print("Você tem {0} reais".format(dinheiro))
+            else:
+                dinheiro-= aposta
+                print('Perdeu, a soma deu {0}'.format(soma_dados_ac))
+                print("Você tem {0} reais".format(dinheiro))
+
+            if dinheiro > 0:
+                opcoes = input("Você quer apostar em field, any_craps ou twelve?")
+
+
+
+        elif qual == "t":
+            aposta = int(input("Quanto você quer apostar?"))
+
+
+            dado1_t = random.randint(1,6)
+            dado2_t = random.randint(1,6)
+            soma_dados_t = dado1_t + dado2_t
+
+
+            if soma_dados_t == 12:
+                dinheiro += aposta*30
+                print('Ganhou, a soma deu {0}'.format(soma_dados_t))
+                print("Você tem {0} reais".format(dinheiro))
+            else:
+                dinheiro -= aposta
+                print('Perdeu, a soma deu {0}'.format(soma_dados_t))
+                print("Você tem {0} reais".format(dinheiro))
+
+            if dinheiro > 0:
+                opcoes = input("Você quer apostar em field, any_craps ou twelve?")
+
+    if dinheiro <= 0:
+        break
+
+
     print(" ")    
     print("Você está no Come Out")
     aposta = int(input("Quanto você quer apostar?"))
@@ -135,23 +115,27 @@ while dinheiro > 0:
         
     else:       
         print("A soma dos dados deu {0}".format(soma_dados))
+        print(" ") 
         print('Você está na fase point')
+        print(" ") 
         point = soma_dados
         print("A soma dos dados precisa dar {0}".format(point))
+        print(" ") 
         soma_dados_point = random.randint(2,12)
         
         while soma_dados_point != point and soma_dados_point!= 7:
             soma_dados_point = random.randint(2,12)
-            print("A soma dos dados deu {0}".format(soma_dados_point))
-            
+            print("A soma dos dados deu {0}".format(soma_dados_point)) 
+
         if soma_dados_point == 7:
             print("A soma dos dados deu {0}".format(soma_dados_point))
             dinheiro -= aposta 
             print("Você tem {0} reais".format(dinheiro))
+            print(" ") 
             
         elif soma_dados_point == point:
-            print("A soma dos dados deu {0}".format(soma_dados_point))
             dinheiro += aposta 
             print("Você tem {0} reais".format(dinheiro))
+            print(" ") 
 
 print('Acabou! Você faliu! :(' )
